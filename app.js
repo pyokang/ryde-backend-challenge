a65da53c9b836d1b15d2f79d34b30d330f62ab2c
Kang Pyo <PYO001@e.ntu.edu.sg>
2021-08-05 23:03:12 +0900

    Installed Ajv for JSON validation; Made correction to the insert such that it checks if data exists

--- app.js
+++ app.js
@@ -3,14 +3,34 @@ const express = require("express");
 // const router = require("./routes/crud");
 const bodyParser = require("body-parser");
 const { MongoClient } = require("mongodb");
+const Ajv = require("ajv");
 require("dotenv").config();
 
 uri = process.env.DB_CONNECTION;
+
+// AJV JSON validator
+const ajv = new Ajv();
+
+// TODO: change dob and createdAt datatype to date
+const schema = {
+  type: "object",
+  properties: {
+    id: { type: "string" },
+    name: { type: "string" },
+    dob: { type: "string" },
+    address: { type: "string" },
+    description: { type: "string" },
+    createdAt: { type: "string" },
+  },
+  required: ["id", "name", "dob", "address", "description", "createdAt"],
+};
+const validate = ajv.compile(schema);
 
 var app = express();
 
 app.use(bodyParser.json());
 
+// Reusable DB access function
 const withDB = async (operations, res) => {
   try {
     const client = new MongoClient(uri, {
@@ -48,11 +68,16 @@ app.get("/api/userinfo/:id", async (req, res) => {
 
 // Insert one item
 app.post("/api/userinfo/", async (req, res) => {
-  const userId = req.params.id;
+  const userId = req.body.id;
   // TO DO: Validate the structure of the data
+  const valid = validate(req.body);
+  console.log(req.body, valid);
+  if (!valid)
+    return res
+      .status(400)
+      .json({ message: "Check if the data fields are correct." });
   withDB(async (collection) => {
-    const item = await collection.findOne({ id: userId });
-    if (!item) {
+    if ((await collection.countDocuments({ id: userId })) != 0) {
       return res
         .status(400)
         .json({ message: "There already exist entry with the id" });
